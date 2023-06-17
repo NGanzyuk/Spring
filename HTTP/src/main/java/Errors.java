f5d7cef4b539d4f351d656e2e86a92dd444f0550
Ganzyuk Nikita <nganzyuk.edu@gamail.com>
2023-06-17 23:05:56 +0300

    Initial commit

---
+++ HTTP/src/main/java/Errors.java
@@ -0,0 +1,19 @@
+public class Errors {
+    public static String returnError(int error, String mimeType, long length){
+        if (error == 200){
+            return "HTTP/1.1 "+ error +" OK\r\n" +
+                    "Content-Type: " + mimeType + "\r\n" +
+                    "Content-Length: " + length + "\r\n" +
+                    "Connection: close\r\n" +
+                    "\r\n";
+        }
+        if(error == 404){
+           return  "HTTP/1.1 404 Not Found\r\n" +
+                    "Content-Length: 0\r\n" +
+                    "Connection: close\r\n" +
+                    "\r\n";
+        }
+
+        return "";
+    }
+}
---
+++ HTTP/src/main/java/Server.java
@@ -0,0 +1,78 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Server {
+    private final int port = 9999;
+    private final List validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html",
+            "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+
+    public Server() {
+
+    }
+
+    public void start() {
+
+        try (final var serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try (
+                        final var socket = serverSocket.accept();
+                        final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        final var out = new BufferedOutputStream(socket.getOutputStream());
+                ) {
+
+                    final var requestLine = in.readLine();
+                    final var parts = requestLine.split(" ");
+                    if (parts.length != 3) {
+                        continue;
+                    }
+
+                    final var path = parts[1];
+                    if (!validPaths.contains(path)) {
+                        out.write((
+                                Errors.returnError(404, "", 0)
+                        ).getBytes());
+                        out.flush();
+                        continue;
+                    }
+
+                    final var filePath = Path.of(".", "public", path);
+                    final var mimeType = Files.probeContentType(filePath);
+
+                    if (path.equals("/classic.html")) {
+                        final var template = Files.readString(filePath);
+                        final var content = template.replace(
+                                "{time}",
+                                LocalDateTime.now().toString()
+                        ).getBytes();
+                        out.write((
+                                Errors.returnError(200, mimeType, content.length)
+                        ).getBytes());
+                        out.write(content);
+                        out.flush();
+                        continue;
+                    }
+
+                    final var length = Files.size(filePath);
+                    out.write((
+                            Errors.returnError(200, mimeType, length)
+                    ).getBytes());
+                    Files.copy(filePath, out);
+                    out.flush();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
+
