f90b01c44483e47e171bc108b039af937c4e581b
Ganzyuk Nikita <nganzyuk.edu@gamail.com>
2023-06-18 18:12:51 +0300

    Fix

--- HTTP/src/main/java/Main.java
+++ HTTP/src/main/java/Main.java
@@ -1,6 +1,19 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class Main {
     public static void main(String[] args) {
-        var server = new Server();
-        server.start();
+        Callable<Integer> task = () -> {
+            var server = new Server(9999);
+            server.start();
+            return 0;
+        };
+        ExecutorService threadPool = Executors.newFixedThreadPool(64);
+        for (int i = 0; i < 64; i++) {
+            Future result = threadPool.submit(task);
+        }
+        threadPool.shutdown();
     }
 }
